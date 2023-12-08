76b41056c2188402527140589d79573d4173a808
RuanNunes <ruanschwanz@gmail.com>
2023-12-07 22:01:12 -0300

    first commit

---
+++ src/main/java/ruan/nunes/springfiap/SpringFiapApplication.java
@@ -0,0 +1,13 @@
+package ruan.nunes.springfiap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFiapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFiapApplication.class, args);
+	}
+
+}
